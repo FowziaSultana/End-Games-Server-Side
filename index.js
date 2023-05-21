3e71e3ccd1e4a9f97b6285b76a6a2fe9b32898b0
FowziaSultana <fowziasultana168@gmail.com>
2023-05-21 22:37:44 +0600

    update a call

--- index.js
+++ index.js
@@ -166,7 +166,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send(`Toy App is running ${pass}`);
+  res.send(`Toy App is running `);
 });
 
 app.listen(port, () => {
